24531fed0343d28c127958a5277c420bd018f3f1
soutammolick829 <soutammollick829@gmail.com>
2023-02-27 02:23:45 +0600

    complete waiting

--- js/hunter.js
+++ js/hunter.js
@@ -7,6 +7,15 @@ const lodePhoneItems = async(searchText) =>{
 const displayPhones = phones =>{
     const phonesContainer = document.getElementById(`phones-container`);
     phonesContainer.textContent = "";
+    const showAll = document.getElementById(`show-all-btn`);
+    if(phones.length > 6){
+        phones = phones.slice(0,6);
+        showAll.classList.remove(`d-none`);
+    }
+    else{
+        showAll.classList.add(`d-none`);
+    }
+    
     // display no phones found....
     const alertMassage = document.getElementById(`alert-massage`);
     if(phones.length === 0){
@@ -15,9 +24,7 @@ const displayPhones = phones =>{
     else{
         alertMassage.classList.add(`d-none`);
     }
-
-
-
+// all phones ...
     phones.forEach(phones =>{
         const phoneDiv = document.createElement(`div`);
         phoneDiv.classList.add(`col`);
@@ -32,12 +39,26 @@ const displayPhones = phones =>{
     </div>
     </div>`
     phonesContainer.appendChild(phoneDiv);
-    })
+    });
+    // close append child 
+    toggleSpinner();
 }
 document.getElementById(`btn-search-phone`).addEventListener(`click`, function(){
+    toggleSpinner(true);
     const searchInputField = document.getElementById(`Search-field`);
     const searchText = searchInputField.value;
     lodePhoneItems(searchText);
 })
 
+const toggleSpinner = isLoading =>{
+    const loaderSection = document.getElementById(`loader`);
+    if(isLoading){
+        loaderSection.classList.remove(`d-none`);
+    }
+    else{
+        loaderSection.classList.add(`d-none`);
+    }
+}
+
 // lodePhoneItems();
+
